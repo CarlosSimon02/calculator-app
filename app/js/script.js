0777e428904b60edf1c3df0b4c2021440e49ea32
CarlosSimon02 <119652306+CarlosSimon02@users.noreply.github.com>
2023-02-23 11:00:55 +0800

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -24,18 +24,78 @@ let changeTheme = function () {
 
 let expression = document.getElementById("screen");
 
+let isMathError = function() {
+    if(expression.innerHTML == NaN || 
+       expression.innerHTML == Infinity ||
+       expression.innerHTML == -Infinity ||
+       expression.innerHTML == "Math Error") return true;
+}
+
+let isOperation = function(value) {
+    if(value === "+" ||
+       value === "/" ||
+       value === "*" ||
+       value === "-") return true;
+}
+
 let typeChar = function(value) {
+    if(value === ".") { 
+        if(isOperation(expression.innerHTML.slice(-1))) {
+            expression.innerHTML += " 0.";
+            return;
+        } 
+        
+        try {
+            Function(`return ` + expression.innerHTML + value)()
+        } catch(error) {
+            return;
+        }
+
+    } else if(isMathError() || expression.innerHTML === "0") {
+        if(value === ".") {
+            expression.innerHTML = "0.";
+            return;
+        }
+        expression.innerHTML = value;
+        return;
+    } else if(isOperation(value) || isOperation(expression.innerHTML.slice(-1))) {
+        expression.innerHTML += " " + value;
+        return;
+    } else if(expression.innerHTML.slice(-1) == "0" && expression.innerHTML.slice(-2,-1) == " ") {
+        if(value === ".") {
+            expression.innerHTML += "0.";
+            return;
+        }
+
+        expression.innerHTML = expression.innerHTML.slice(0,-1);
+        expression.innerHTML += value;
+        return;
+    }
+
     expression.innerHTML += value;
 }
 
 let del = function() {
+    if(isMathError()) {
+        expression.innerHTML = "0";
+        return;
+    } else if(expression.innerHTML.slice(-2,-1) == " " ) {
+        expression.innerHTML = expression.innerHTML.slice(0,-1);
+    }
+    
     expression.innerHTML = expression.innerHTML.slice(0,-1);
+    if(expression.innerHTML === "") expression.innerHTML = "0";
 }
 
 let equal = function() {
-    expression.innerHTML = Function(`return ` + expression.innerHTML)();
+    try {
+        expression.innerHTML = Function(`return ` + expression.innerHTML)();
+    }
+    catch(error) {
+        expression.innerHTML = "Math Error";
+    }
 }
 
 let reset = function() {
-    expression.innerHTML = "";
+    expression.innerHTML = "0";
 }
